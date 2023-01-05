2e8772a8715f3835e683bc321fe64c25da5ce3ad
Akshith Rao <113459723+akshith-3@users.noreply.github.com>
2023-01-05 15:19:25 +0530

    Add files via upload

---
+++ 20220927162227-create-todo.js
@@ -0,0 +1,42 @@
+/* eslint-disable no-undef */
+
+/* eslint-disable no-unused-vars */
+"use strict";
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable("Todos", {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      title: {
+        type: DataTypes.STRING,
+        allowNull:false,
+        validate:{
+          notNull:true,
+          len:5,
+        },
+      },
+      dueDate: {
+        type: Sequelize.DATEONLY,
+      },
+      completed: {
+        type: Sequelize.BOOLEAN,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable("Todos");
+  },
+};
---
+++ todo.js
@@ -0,0 +1,99 @@
+/* eslint-disable no-undef */
+/* eslint-disable no-unused-vars */
+"use strict";
+const { Model, Op } = require("sequelize");
+module.exports = (sequelize, DataTypes) => {
+  class Todo extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      Todo.belongsTo(models.User, {
+        foreignKey:'userId'
+      })
+      // define association here
+    }
+
+    static async overdue(userId) {
+      return await Todo.findAll({
+        where: {
+          dueDate: { [Op.lt]: new Date().toLocaleDateString("en-CA") },
+          userId,
+          completed:false,
+        },
+        order:[["id","ASC"]],
+      });
+    }
+
+    static async dueToday(userId,) {
+      return await Todo.findAll({
+        where: {
+          dueDate: { [Op.eq]: new Date().toLocaleDateString("en-CA") },
+          userId,
+          completed:false,
+        },
+        order:[["id","ASC"]],
+      });
+    }
+
+    static async dueLater(userId,) {
+      return await Todo.findAll({
+        where: {
+          dueDate: { [Op.gt]: new Date().toLocaleDateString("en-CA") },
+          userId,
+          completed:false,
+        },
+        order:[["id","ASC"]],
+      });
+    }
+
+    static async completedTodo(userId,){
+      return await Todo.findAll({
+        where:{
+          completed:true,
+          userId
+        }
+      })
+    }
+
+    setCompletionStatus(completed){
+      console.log(completed);
+      return this.update({completed:completed});
+    }
+
+    static getTodos(){
+      return this.findAll();
+    }
+    static addTodo({ title, dueDate ,userId}) {
+      return this.create({ title: title, dueDate: dueDate, completed: false,userId });
+    }
+
+    markAsCompleted() {
+      return this.update({ completed: true });
+    }
+
+    static async remove(id,userId) {
+      return this.destroy({
+        where: {
+          id: id,
+          userId
+        },
+      });
+    }
+
+  }
+  Todo.init(
+    {
+      title: DataTypes.STRING,
+      dueDate: DataTypes.DATEONLY,
+      completed: DataTypes.BOOLEAN,
+    },
+    {
+      sequelize,
+      modelName: "Todo",
+    }
+  );
+  return Todo;
+};
